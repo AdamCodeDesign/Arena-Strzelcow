fb6753541225c9de846d4d5ac22429d6a25b00ee
LeshchuDev <leshchudev@gmail.com>
2025-04-13 10:56:44 +0200

    docker connected with prisma done

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,7 @@
-
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+console.log("databaseURL", process.env.DATABASE_URL);
 
 export default prisma;
