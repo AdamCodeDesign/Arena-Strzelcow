f30397ae18c25ff9ba89a6f27593c78005f6af8c
LeshchuDev <leshchudev@gmail.com>
2025-04-15 19:31:45 +0200

    basic file structure for frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-
 export default function HomePage() {
     return (
-      <main>
-        <h1>Witaj w aplikacji Arena Strzelców!</h1>
-        <p>Platforma do śledzenia wyników w strzelectwie.</p>
-      </main>
+        <main>
+            <h1>Witaj w aplikacji Arena Strzelców!</h1>
+            <p>Platforma do śledzenia wyników w strzelectwie.</p>
+        </main>
     );
-  }
-  
+}
